228f782a85271a7074fbf76ff62bb0d5672ae2a9
ranaaditya <mail2aditya.rana98@gmail.com>
2020-03-09 18:54:39 +0530

    Add ios binarie for XCode

--- SharedKotlinCode/build.gralde.kts
+++ SharedKotlinCode/build.gralde.kts
@@ -1,13 +1,11 @@
-
-#!/usr/bin/env bash
-
 import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 
-        plugins {
-            kotlin("multiplatform")
-        }
+plugins {
+    kotlin("multiplatform")
+}
 
 kotlin {
+    //select iOS target platform depending on the Xcode environment variables
     val iOSTarget: (String, KotlinNativeTarget.() -> Unit) -> KotlinNativeTarget =
         if (System.getenv("SDK_NAME")?.startsWith("iphoneos") == true)
             ::iosArm64
@@ -32,3 +30,34 @@ kotlin {
         implementation("org.jetbrains.kotlin:kotlin-stdlib")
     }
 }
+
+val packForXcode by tasks.creating(Sync::class) {
+    val targetDir = File(buildDir, "xcode-frameworks")
+
+    /// selecting the right configuration for the iOS 
+    /// framework depending on the environment
+    /// variables set by Xcode build
+    val mode = System.getenv("CONFIGURATION") ?: "DEBUG"
+    val framework = kotlin.targets
+        .getByName<KotlinNativeTarget>("ios")
+        .binaries.getFramework(mode)
+    inputs.property("mode", mode)
+    dependsOn(framework.linkTask)
+
+    from({ framework.outputDirectory })
+    into(targetDir)
+
+    /// generate a helpful ./gradlew wrapper with embedded Java path
+    doLast {
+        val gradlew = File(targetDir, "gradlew")
+        gradlew.writeText(
+            "#!/bin/bash\n"
+                    + "export 'JAVA_HOME=${System.getProperty("java.home")}'\n"
+                    + "cd '${rootProject.rootDir}'\n"
+                    + "./gradlew \$@\n"
+        )
+        gradlew.setExecutable(true)
+    }
+}
+
+tasks.getByName("build").dependsOn(packForXcode)
